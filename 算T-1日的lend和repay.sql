d5fee5421bcd8bc575cba7bc867bfb2d3d92eb68
swartt <1607385904@qq.com>
2026-08-14 12:53:26 +0800

    删除无关代码

--- 算T-1日的lend和repay.sql
+++ 算T-1日的lend和repay.sql
@@ -9,20 +9,3 @@ GROUP BY
     DATE(transaction_time),
     transaction_type
 ORDER BY transaction_type;
-
-
-
-
-
-
-
-
-
-SELECT
-    DATE(transaction_time) AS `日期`,
-    COUNT(*) AS `放款笔数`,
-    SUM(principal_amount) AS `用信金额`
-FROM ug_analysis_dw_prod.ods_mtn_b_trans_record
-WHERE DATE(transaction_time) = '2026-08-11'
-  AND transaction_type = 'Overdraft Funding'
-GROUP BY DATE(transaction_time);
